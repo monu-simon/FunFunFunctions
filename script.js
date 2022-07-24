dc3972cfbaf124bd7a17f34e4a64ce896fbce7e8
monu-simon <monusimonm@gmail.com>
2022-07-24 12:51:07 +0530

    Prototype

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 function talk() {
-    console.log(this.sound);
+    console.log(this)
+    console.log(this.sound)
 }
 
-let me = {
-    sound: 'Hello....'
+let animal = {
+    talk: talk
 }
-talk(); // undefined - sound is a global variable
-let talkBoungToMe = talk.bind(me);
-talkBoungToMe();
+
+let dog = {
+    sound: 'Woof'
+}
+
+let husky = {
+    howl: function() {
+        console.log(this.sound.toUpperCase());
+    }
+}
+
+Object.setPrototypeOf(dog, animal); // Sets the property of animal to cat
+animal.talk = function() { //We are changing the talk property after setting it to dog
+    console.log('Changing the talk function.')
+}
+dog.talk(); //Will output 'Changing the talk function' since a refrence is only kept. We are not creating a new function or anything
+Object.setPrototypeOf(husky, dog); //Deligation
+husky.howl()
